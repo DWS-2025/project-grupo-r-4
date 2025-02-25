30122b51bc1abb79ae9bd46477438633357bc4e5
hugomn4 <hugo.mn2004@gmail.com>
2025-02-25 11:37:29 +0100

    ReviewService

--- Practica/Practica/src/main/java/com/example/demo/Service/ReviewService.java
+++ Practica/Practica/src/main/java/com/example/demo/Service/ReviewService.java
@@ -1,4 +1,31 @@
 package com.example.demo.Service;
 
+import com.example.demo.Model.Review;
+
+import java.util.Optional;
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.atomic.AtomicLong;
+
 public class ReviewService {
+    private ConcurrentHashMap<Long, Review> reviews = new ConcurrentHashMap<>();
+    private AtomicLong nextId = new AtomicLong();
+
+    public Optional<Review> findById(long id) {
+        if(this.reviews.containsKey(id)){
+            return Optional.of(reviews.get(id));
+        }else
+            return Optional.empty();
+    }
+
+    public Review save(Review review) {
+        long id = nextId.getAndIncrement();
+        review.setId(id);
+        reviews.put(id, review);
+        return review;
+
+    }
+
+    public void deleteById(long id) {
+        this.reviews.remove(id);
+    }
 }
