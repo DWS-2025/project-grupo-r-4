8944cab43afef347d80f38ccba58ff08f7f5cbae
Beatriz <beatriz.sanz.granados@gmail.com>
2025-03-18 09:53:59 +0100

    Update DemoApplication.java

--- Practica/Practica/src/main/java/com/example/demo/DemoApplication.java
+++ Practica/Practica/src/main/java/com/example/demo/DemoApplication.java
@@ -3,7 +3,7 @@ package com.example.demo;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-// Commit de prueba
+
 @SpringBootApplication
 public class DemoApplication {
 
